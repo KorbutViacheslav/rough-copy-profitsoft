0f04b088633dd5eddbf27edb04ba17801b260c3f
Viacheslav Korbut <minowaman777@gmail.com>
2024-04-10 15:58:24 +0300

    Created first test and added dependency JUnit 5

--- src/test/java/org/profitsoft/service/StatisticsCalculatorTest.java
+++ src/test/java/org/profitsoft/service/StatisticsCalculatorTest.java
@@ -1,7 +1,5 @@
 package org.profitsoft.service;
 
-import junit.framework.TestCase;
-
 /**
  * Author: Viacheslav Korbut
  * Date: 10.04.2024
--- src/test/java/org/profitsoft/util/StatisticsFileNameGeneratorTest.java
+++ src/test/java/org/profitsoft/util/StatisticsFileNameGeneratorTest.java
@@ -1,8 +1,21 @@
 package org.profitsoft.util;
 
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
 /**
  * Author: Viacheslav Korbut
  * Date: 10.04.2024
  */
 public class StatisticsFileNameGeneratorTest {
+
+    @Test
+    void testGetXMLFileName(){
+        String actual = StatisticsFileNameGenerator.getXMLFileName("author");
+        String expected = "src/main/resources/statistic_results/statistics_by_author.xml";
+        assertEquals(expected,actual);
+    }
 }
